f27029b5b3d5e9bead576fc8e13124eaf5c21e29
qatatonic <qhoang4@gmail.com>
2024-08-22 23:16:30 -0700

    hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Welcome to Quyen Voyles UX")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}
